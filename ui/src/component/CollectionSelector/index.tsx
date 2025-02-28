3bbd1d405195d947fdfdd7912cec245fbef6648a
as <allensnape@gmail.com>
2025-02-28 16:18:37 +0800

    refactor: refine

--- ui/src/component/CollectionSelector/index.tsx
+++ ui/src/component/CollectionSelector/index.tsx
@@ -1,11 +1,10 @@
 import { BaseSearchParams } from "@allape/gocrud";
-import { CrudySelector, ILV } from "@allape/gocrud-react";
+import { CrudySelector } from "@allape/gocrud-react";
 import { ICrudySelectorProps } from "@allape/gocrud-react/src/component/CrudySelector";
-import { PropsWithChildren, ReactElement, useCallback, useMemo } from "react";
+import { PropsWithChildren, ReactElement, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { CollectionCrudy } from "../../api/collection.ts";
 import {
-  CollectionTypes,
   ICollection,
   ICollectionSearchParams,
 } from "../../model/collection.ts";
@@ -18,25 +17,6 @@ export default function CollectionSelector(
   props: PropsWithChildren<ICollectionSelectorProps>,
 ): ReactElement {
   const { t } = useTranslation();
-
-  const handleBuild = useCallback(
-    (records: ICollection[]) => {
-      const groups: ILV<ICollection["type"]>[] = CollectionTypes.map((i) => ({
-        ...i,
-      }));
-      groups.forEach((g) => {
-        g.label = t(g.label as string);
-        g.options = records
-          .filter((r) => r.type === g.value)
-          .map((r) => ({
-            label: `${r.id}: ${r.name}`,
-            value: r.id,
-          }));
-      });
-      return groups as unknown as ILV<ICollection["id"]>[];
-    },
-    [t],
-  );
 
   const sp = useMemo<ICollectionSearchParams>(
     () => ({
@@ -54,7 +34,6 @@ export default function CollectionSelector(
       pageSize={1000}
       searchParams={sp}
       searchPropName="keywords"
-      buildLV={handleBuild}
     />
   );
 }
